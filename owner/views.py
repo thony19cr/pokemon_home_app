0069a8be494a45d89f58fd4e21a07d3c59bea809
“Anthony <acarrillorj@gmail.com>
2023-02-08 22:02:10 -0500

    config about urls

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def list_owner(request):
+    print("Probando vista!!")
+    name = "CERSEU"
+    return HttpResponse(name)
